612ebeb046985b5c754f259f0b2fb61ec1b6e9a7
Akshit Choudhary <Akkii4>
2020-12-29 19:00:57 +0530

    Latest Fixes

--- contracts/LinearVesting.sol
+++ contracts/LinearVesting.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.16;
+
+pragma solidity ^0.6.0;
 
 library SafeMath {
     /**
@@ -208,14 +209,41 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract ReentrancyGuard {
-    // counter to allow mutex lock with only one SSTORE operation
-    uint256 private _guardCounter;
+/**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ *
+ * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
+ * available, which can be applied to functions to make sure there are no nested
+ * (reentrant) calls to them.
+ *
+ * Note that because there is a single `nonReentrant` guard, functions marked as
+ * `nonReentrant` may not call one another. This can be worked around by making
+ * those functions `private`, and then adding `external` `nonReentrant` entry
+ * points to them.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ */
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
 
     constructor () internal {
-        // The counter starts at one to prevent changing it from zero to a non-zero
-        // value, which is a more expensive operation.
-        _guardCounter = 1;
+        _status = _NOT_ENTERED;
     }
 
     /**
@@ -226,10 +254,17 @@ contract ReentrancyGuard {
      * `private` function that does the actual work.
      */
     modifier nonReentrant() {
-        _guardCounter += 1;
-        uint256 localCounter = _guardCounter;
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
         _;
-        require(localCounter == _guardCounter, "ReentrancyGuard: reentrant call");
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
     }
 }
 
@@ -349,7 +384,8 @@ contract LinearVesting is ReentrancyGuard {
     // Accessors
 
     /**
-     * @notice Vested token balance 
+     * @notice Vested token balance for a beneficiary
+     * @dev Must be called directly by the beneficiary assigned the tokens in the schedule
      * @return _tokenBalance total balance proxied via the ERC20 token
      */
     function tokenBalance() external view returns (uint256) {
@@ -358,6 +394,7 @@ contract LinearVesting is ReentrancyGuard {
 
     /**
      * @notice Vesting schedule and associated data for a beneficiary
+     * @dev Must be called directly by the beneficiary assigned the tokens in the schedule
      * @return _amount
      * @return _totalDrawn
      * @return _lastDrawnAt
